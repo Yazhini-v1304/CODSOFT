55a60ad07b526f8928344abaf8ebb4f5809086f4
Yazhini-v1304 <yazhiniv2k6@gmail.com>
2025-06-19 21:16:57 +0530

    simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,29 @@
+def calculator():
+  printf("Welcome to the Simple Calculator!")
+  try:
+    num1 = float(input("Enter the first number:"))
+    num2 = float(input("Enter the second number:"))
+    print("Choose an operation:")
+    print("1.Addition(+)")
+    print("2.Subtraction(-)")
+    print("3.Multiplication(*)")
+    print("4.Division(/)")
+    operation = input("Enter the operation symbol(+,-,*,/):")
+    if operation == '+':
+      result = num1 + num2
+    elif operation =='-':
+      result = num1-num2
+    elif operation =='*':
+      result = num1*num2
+    elif operation =='/':
+      if num2 == 0:
+        print("Error: Cannot divide by zero.")
+        return
+        result = num1 / num2 
+      else:
+        print("Invalid operation choice.")
+        return
+        print(f"The result of {num1} {operation} {num2} ={result}")
+  except ValueError:
+    print("Invalid input. Please enter numeric values.")
+    calculator()
